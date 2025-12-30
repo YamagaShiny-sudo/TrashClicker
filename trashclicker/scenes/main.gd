acbe1ddc5a22c5c4195e3e92d81abad6d22ba12b
YamagaShiny <adrien.perrin01@gmail.com>
2025-12-30 12:58:09 +0100

    Base of the game
    Working incremental with main button and upgrade
    - The upgrade are the power of the main click and a passive one
    This commit is adding the brut base game

---
+++ trashclicker/scenes/main.gd
@@ -0,0 +1,45 @@
+extends Control
+
+@onready var score_label: Label = $MarginContainer/VBoxContainer/ScoreLabel
+
+var trash_score = 0
+
+func _ready() -> void:
+	update_text()
+
+func update_text() -> void:
+	score_label.text = "Trash collected: " + str(trash_score)
+	power_label.text = "Power \nCost: " + str(power_cost) + "\nOutput: " + str(output)
+	build_label_1.text = "Building 1 \nCost: " + str(build_cost_1) + "\nOutput: " + str(build_output_1)
+
+var output = 1
+func _on_texture_button_pressed() -> void:
+	trash_score += output
+	update_text()
+
+var power_cost = 2
+@onready var power_label: Label = $MarginContainer/VBoxContainer/PanelContainer/MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Power/PowerLabel
+func _on_power_upgrade_button_pressed() -> void:
+	if trash_score >= power_cost:
+		trash_score -= power_cost
+		output += 1
+		power_cost += 1
+		update_text()
+
+var build_cost_1 = 2
+var build_output_1 = 0
+@onready var build_timer_1: Timer = $MarginContainer/VBoxContainer/PanelContainer/MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Building1/BuildTimer1
+@onready var build_label_1: Label = $MarginContainer/VBoxContainer/PanelContainer/MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Building1/BuildLabel1
+func _on_build_upgrade_button_1_pressed() -> void:
+	if trash_score >= build_cost_1:
+		trash_score -= build_cost_1
+		build_output_1 += 1
+		build_cost_1 += 1
+		build_timer_1.start()
+		update_text()
+		
+
+func _on_timer_timeout() -> void:
+	trash_score += build_output_1
+	update_text()
+	build_timer_1.start()
