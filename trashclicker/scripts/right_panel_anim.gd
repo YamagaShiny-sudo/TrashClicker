839bd36542cbbb1e75de2a7b65b18004858cb61b
YamagaShiny <adrien.perrin01@gmail.com>
2026-01-01 02:46:59 +0100

    Slider animation
    -Made an animation for the settings and upgrades menu

---
+++ trashclicker/scripts/right_panel_anim.gd
@@ -0,0 +1,24 @@
+extends Panel
+
+@onready var slide_animation: AnimationPlayer = $slideAnimation
+
+func _ready() -> void:
+	slide_animation.play("slide_out_right")
+
+@onready var button: Button = $Button
+@onready var disable_time: Timer = $disableTime
+var close = true
+func _on_button_pressed() -> void:
+	if close:
+		button.disabled = true
+		slide_animation.play("slide_in_right")
+		close = false
+		disable_time.start()
+		
+	else:
+		slide_animation.play("slide_out_right")
+		close = true
+
+
+func _on_disable_time_timeout() -> void:
+	button.disabled = false
