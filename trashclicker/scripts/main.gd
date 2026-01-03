c09557596eccf0a1e2dd30bc3660583ecaed848f
YamagaShiny <adrien.perrin01@gmail.com>
2026-01-03 20:37:28 +0100

    CleanUP and comments
    -Added some comments and a bit of cleanup inside the main.gd script

--- trashclicker/scripts/main.gd
+++ trashclicker/scripts/main.gd
@@ -1,12 +1,18 @@
-extends Control
+extends Node
 
 @onready var score_label: Label = $ScoreLabel
 
 var trash_score = 0
 
+
+
+#Initialising at the start
 func _ready() -> void:
 	update_text()
 
+
+
+#Updating the label
 func update_text() -> void:
 	format_score(trash_score)
 	power_label.text = "Power \nCost: " + str(power_cost) + "\nOutput: " + str(output)
@@ -14,35 +20,36 @@ func update_text() -> void:
 
 var output = 1
 func _main_button_pressed() -> void:
-	trash_score += output
-	update_text()
+	addScore(output)
 
 var power_cost = 2
 @onready var power_label: Label = $RightMargin/RightPanel/ScrollUpgrade/BoxUpgrade/Power/PowerLabel
 func _on_power_upgrade_button_pressed() -> void:
 	if trash_score >= power_cost:
-		trash_score -= power_cost
+		subScore(power_cost)
 		output += 1
 		power_cost += 1
-		update_text()
 
+
+
+#Building insitialisation
 var build_cost_1 = 2
 var build_output_1 = 0
 @onready var build_timer: Timer = %BuildTimer
 @onready var build_label_1: Label = $RightMargin/RightPanel/ScrollUpgrade/BoxUpgrade/Building1/BuildLabel1
 func _on_build_upgrade_button_1_pressed() -> void:
 	if trash_score >= build_cost_1:
-		trash_score -= build_cost_1
+		subScore(build_cost_1)
 		build_output_1 += 1
 		build_cost_1 += 1
 		build_timer.start()
-		update_text()
-
-
+#Building adding score
 func _on_build_timer_timeout() -> void:
-	trash_score += build_output_1
-	update_text()
-	
+	addScore(build_output_1)
+
+
+
+#Make the score 1000 to 1k and etc
 func format_score(score : int) -> void:
 	var lenght_score = len(str(score))
 	if lenght_score < 4:
@@ -58,25 +65,41 @@ func format_score(score : int) -> void:
 	elif lenght_score < 19:
 		score_label.text = "Trash collected: " + str("%.2f" % (float(score)/10**15), "Quad")
 
+
+
+#AutoClicker PowerUP
 @onready var auto_stop: Timer = %AutoStop
 @onready var auto_clicker_timer: Timer = %AutoClickerTimer
 func auto_clicker() -> void:
 	auto_clicker_timer.start()
 	auto_stop.start()
-
+#Press the main button
 func _on_auto_clicker_timer_timeout() -> void:
 	_main_button_pressed()
-
+#Stop the AutoClicker
 func _on_auto_stop_timeout() -> void:
 	auto_clicker_timer.stop()
 	auto_stop.stop()
-	
 
+
+
+#Speed Building PowerUP
 @onready var speed_building_stop: Timer = %SpeedBuildingStop
 func speed_building_power() -> void:
 	build_timer.wait_time /= 2
 	speed_building_stop.start()
-
+#Stop the speed building powerUP
 func _on_speed_building_stop_timeout() -> void:
 	build_timer.wait_time = 1
 	speed_building_stop.stop()
+
+
+
+#Adding to the score
+func addScore(amount:int) -> void:
+	trash_score += amount
+	update_text()
+#Substracting from the score
+func subScore(amount:int) -> void:
+	trash_score -= amount
+	update_text()
