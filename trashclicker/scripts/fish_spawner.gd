9ca80027d26b9510308b5f4466f794096c97fb6d
YamagaShiny <adrien.perrin01@gmail.com>
2026-04-17 22:33:35 +0200

    Update fish_spawner.gd

--- trashclicker/scripts/fish_spawner.gd
+++ trashclicker/scripts/fish_spawner.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var randomTimeSpawn : Array[int] = [1, 2]
+var randomTimeSpawn : Array[int] = [30, 60]
 var spawnObject = preload("res://scenes/fish.tscn")
 
 @onready var spawn_timer: Timer = $SpawnTimer
